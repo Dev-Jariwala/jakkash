02eb1a8c5e11464718e5a6fce5c0a918f520b499
Dev-Jariwala <devjariwala.j@gmail.com>
2024-01-30 15:53:21 +0530

    mobile screen setted

--- src/components/table2/Pagination.jsx
+++ src/components/table2/Pagination.jsx
@@ -13,7 +13,7 @@ const Pagination = ({
 }) => {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 dark:bg-gray-800 dark:text-gray-200 ">
-      <div className="flex flex-1 justify-between sm:hidden">
+      {/* <div className="flex flex-1 justify-between sm:hidden">
         <div
           className="relative inline-flex items-center rounded-md border border-gray-300 bg-white dark:bg-gray-800 dark:text-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
           onClick={() => {
@@ -33,9 +33,9 @@ const Pagination = ({
         >
           Next
         </div>
-      </div>
+      </div> */}
       <div className="flex items-center text-sm justify-between sm:flex sm:flex-1 sm:items-center sm:justify-between">
-        <div>
+        <div className="hidden sm:block">
           <p className="text-sm text-gray-700 dark:text-white mr-5">
             Showing <span className="font-medium">{indexOfFirstRow}</span> to{" "}
             <span className="font-medium">{indexOfLastRow}</span> of{" "}
@@ -125,9 +125,6 @@ const Pagination = ({
                 {currentPage + 3}
               </div>
             )}
-            {/* <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-              ...
-            </span> */}
 
             <div
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 dark:text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
--- src/components/table2/Table2Features.jsx
+++ src/components/table2/Table2Features.jsx
@@ -11,20 +11,6 @@ const Table2Features = ({
 }) => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-      <div className="relative w-14">
-        <select
-          className="block appearance-none w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-          onChange={(e) => setPageSize(e.target.value)}
-        >
-          <option value="5">5</option>
-          <option value="10">10</option>
-          <option value="15">15</option>
-        </select>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 dark:text-gray-400">
-          <span className="material-icons">arrow_drop_down</span>
-        </div>
-      </div>
-
       <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
         <form className="flex items-center">
           <label htmlFor="simple-search" className="sr-only">
@@ -47,13 +33,27 @@ const Table2Features = ({
             />
           </div>
         </form>
-        <div className="flex items-center space-x-3 w-full md:w-auto">
-          <OptionButton
-            filters={filters}
-            selectedFilters={selectedFilters}
-            setSelectedFilters={setSelectedFilters}
-          />
+      </div>
+
+      <div className="flex items-center justify-end space-x-3 w-full md:w-auto">
+        <div className="relative w-14">
+          <select
+            className="block appearance-none w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            onChange={(e) => setPageSize(e.target.value)}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 dark:text-gray-400">
+            <span className="material-icons">arrow_drop_down</span>
+          </div>
         </div>
+        <OptionButton
+          filters={filters}
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+        />
       </div>
     </div>
   );
